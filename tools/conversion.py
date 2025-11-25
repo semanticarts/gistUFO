057237348bf98b6444742192c05fe4402b10c289
Will Mandel <willmandel@protonmail.com>
2025-11-25 10:38:19 -0800

    convert to python file; make suggested changes

---
+++ tools/conversion.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import requests
+import json
+from rdflib import Namespace, URIRef, Literal
+from rdflib.graph import Graph, ConjunctiveGraph, Dataset
+from rdflib.namespace import NamespaceManager, RDF, RDFS, XSD, SKOS, OWL
+import csv
+import re
+
+
+DEBUG = False
+
+dct = Namespace("http://purl.org/dc/terms/")
+gist = Namespace("https://w3id.org/semanticarts/ns/ontology/gist/")
+gistd = Namespace("https://w3id.org/semanticarts/ns/data/gist/")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
+rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+sh = Namespace("http://www.w3.org/ns/shacl#")
+skos = Namespace("http://www.w3.org/2004/02/skos/core#")
+xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
+pipe = Namespace("https://semanticarts.com/data/pipedrive/")
+saox = Namespace("https://taxonomies.semanticarts.com/SemArts/")
+gistx = Namespace("https://w3id.org/semanticarts/ns/ontology/gistx/")
+
+g = Dataset()
+
+g.bind("dct", dct)
+g.bind("gist", gist)
+g.bind("gistd", gistd)
+g.bind("owl", owl)
+g.bind("rdf", rdf)
+g.bind("rdfs", rdfs)
+g.bind("sh", sh)
+g.bind("skos", skos)
+g.bind("xsd", xsd)
+g.bind("pipe",pipe)
+g.bind("saox", saox)
+g.bind("gistx", gistx)
+
+special = re.compile("[^A-Za-z0-9-:]")
+
+
+
+UFO_file = Path("./data/gist to UFO Stereotype(class stereotypes).csv")
+with open(UFO_file.absolute()) as csvfile:
+    reader=csv.DictReader(csvfile)
+    for row in reader:
+        if DEBUG:  print(row)
+        gist_class = row.get("gist class")
+        rigidity = row.get("Rigidity(consensus)")
+        identity = row.get("Provides Identity (consensus)")
+
+        gist_class_trimmed = gist_class[5:]
+        gist_class_iri = URIRef(f"https://w3id.org/semanticarts/ns/ontology/gist/{gist_class_trimmed}")
+
+        if rigidity != "":
+            rigidity_trimmed = "_Rigidity_" + re.sub("-", "",(rigidity.lower()))
+            rigidity_iri = URIRef(f"https://w3id.org/semanticarts/ns/ontology/gistx/{rigidity_trimmed}")
+            g.add((gist_class_iri, gist.isCategorizedBy, rigidity_iri))
+
+
+output_file = Path("./data/gistUFOTriples.ttl")
+g.serialize(output_file)
